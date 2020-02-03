2f99e967b4ae525d44e5d718c9c6dc5fe4af12f5
Jalal Mostafa <jalalmostafa22@gmail.com>
2020-02-03 13:05:21 +0000

    minor cli changes

--- timeds.py
+++ timeds.py
@@ -3,20 +3,16 @@ import sys
 import config as conf
 from log import Log
 from replicator import SchemeReplicator
-from pid.decorator import pidfile
 
 
-@pidfile('timeds.pid', piddir='/tmp')
 def main():
     parser = argparse.ArgumentParser(
         description='Replicate databases using Timestamps in SQL Tables')
-    parser.add_argument('--config', default='conf.json',
+    parser.add_argument('--config', '-c', default='conf.json',
                         action='store', help='Set replication configuration file')
-    parser.add_argument('-v', action='store_true',
+    parser.add_argument('--verbose', '-v', action='store_true',
                         help='Verbose Mode. Print config, etc')
-    parser.add_argument('--only-dynamic-and-views', action='store_true',
-                        help='Only replicate dynamic tables and views')
-    parser.add_argument('-d', action='store_true',
+    parser.add_argument('--only-dynamic-and-views', '-d', action='store_true',
                         help='Only replicate dynamic tables and views')
 
     args = parser.parse_args()
@@ -25,12 +21,12 @@ def main():
 
     try:
         config = conf.Config(args.config)
-        if args.v:
+        if args.verbose:
             print(config)
 
         for scheme in config:
             scheme = SchemeReplicator(
-                scheme, config[scheme], only_dynamic_and_views=args.only_dynamic_and_views or args.d)
+                scheme, config[scheme], only_dynamic_and_views=args.only_dynamic_and_views)
             reps = scheme.run()
             replicators.extend(reps)
 
